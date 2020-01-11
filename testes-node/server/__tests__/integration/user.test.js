4ee2085b595f94cd1036a6499ce7610f5669f764
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-11 14:53:51 -0300

    E-mail duplicado

--- testes-node/server/__tests__/integration/user.test.js
+++ testes-node/server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to duplicate email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Vitor Araujo',
+        email: 'diego@rocketseat.com.br',
+        password: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Vitor Araujo',
+        email: 'diego@rocketseat.com.br',
+        password: '123456',
+      });
+
+    expect(response.body).toEqual({ error: 'Duplicated email' });
   });
 });
